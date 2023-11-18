45b3beed74399b6027262113ccfabca3361667ad
RodrigoAvila <rodrii_evil@hotmail.com.ar>
2023-11-17 21:35:29 -0300

    agregado de mnsj, animacion y estilo al contador

--- src/Components/ContadorResponsive.jsx
+++ src/Components/ContadorResponsive.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Flex, Text, Button } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 import popSound from '/assets/sounds/pop.mp3';
 
 const ContadorResponsive = () => {
@@ -10,33 +10,76 @@ const ContadorResponsive = () => {
     const [mostrarMensaje, setMostrarMensaje] = useState(true);
     const [playSound, setPlaySound] = useState(false);
 
-    // Función para obtener una posición aleatoria en la pantalla
+    // Función para obtener una posición aleatoria en el área visible de la pantalla
     const getRandomPosition = () => ({
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight,
+        x: Math.random() * (window.innerWidth - 40),  // Evita que la pelota se salga del lado derecho
+        y: Math.random() * (window.innerHeight - 40), // Evita que la pelota se salga del lado inferior
     });
 
     // Función para obtener un color hexadecimal aleatorio
     const getRandomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
+    // Animación para el contador
+    const contadorAnimation = useAnimation();
+
+    // Animación para el mensaje
+    const mensajeAnimation = useAnimation();
+
+    // Efecto para la animación del contador al incrementar
+    useEffect(() => {
+        contadorAnimation.start({
+            scale: [3, 5],
+            color: ['#3498db', '#e74c3c'],
+            transition: { duration: 0.5 },
+        });
+    }, [contador, contadorAnimation]);
+
+    // Efecto para la animación del mensaje al mostrarse
+    useEffect(() => {
+        mensajeAnimation.start({
+            opacity: [0, 1],
+            y: [0, -50],
+            transition: { duration: 0.5 },
+        });
+    }, [mostrarMensaje, mensajeAnimation]);
+
+    // Función para actualizar el estado de las pelotas
+    const actualizarPelotas = (nuevaPelota) => {
+        setPelotas((prevPelotas) => [...prevPelotas, nuevaPelota]);
+    };
+
     // Función para incrementar el contador y agregar una nueva pelota
     const incrementar = () => {
-        setContador(contador + 1);
+        setContador((prevContador) => prevContador + 1);
         const nuevaPelota = {
             id: pelotas.length + 1,
             position: getRandomPosition(),
             color: getRandomColor(),
-            numero: pelotas.length + 1, // Número de pelota
         };
-        setPelotas([...pelotas, nuevaPelota]);
+        actualizarPelotas(nuevaPelota);
     };
 
     // Función para decrementar el contador y eliminar la última pelota
     const decrementar = () => {
         if (contador > 0) {
-            setContador(contador - 1);
-            setPelotas(pelotas.slice(0, -1));
+            setContador((prevContador) => prevContador - 1);
+            setPelotas((prevPelotas) => prevPelotas.slice(0, -1));
         }
+    };
+
+    // Función para eliminar una pelota por su id
+    const eliminarPelota = (pelotaId) => {
+        setPelotas((prevPelotas) => {
+            const pelotaAEliminar = prevPelotas.find((pelota) => pelota.id === pelotaId);
+
+            if (pelotaAEliminar) {
+                setContador((prevContador) => prevContador - 1);
+                setPlaySound(true);
+                return prevPelotas.filter((pelota) => pelota.id !== pelotaId);
+            }
+
+            return prevPelotas;
+        });
     };
 
     // Función para resetear el contador y eliminar todas las pelotas
@@ -55,27 +98,12 @@ const ContadorResponsive = () => {
         return () => clearTimeout(timeoutId);
     }, []);
 
-    // Función para manejar el clic en una pelota
-    const handleClick = (pelotaId) => {
-        // Busca la pelota por su id
-        const pelotaAEliminar = pelotas.find((pelota) => pelota.id === pelotaId);
-    
-        // Si la encuentra, realiza la resta y elimina la pelota
-        if (pelotaAEliminar) {
-            setContador(contador - 1);
-            setPelotas(pelotas.filter((pelota) => pelota.id !== pelotaId));
-        }
-    
-        // Activa la reproducción del sonido
-        setPlaySound(true);
-    };
-
     // Efecto para reproducir el sonido cuando playSound cambia a true
     useEffect(() => {
         if (playSound) {
             const audio = new Audio(popSound);
             audio.play();
-            setPlaySound(false); // Reinicia el estado para la próxima reproducción
+            setPlaySound(false);
         }
     }, [playSound]);
 
@@ -88,46 +116,43 @@ const ContadorResponsive = () => {
             h="100vh"
             w="100vw"
             overflow="hidden"
+            fontFamily="Comic Sans MS"
         >
             <Flex direction="column" align="center" justify="center">
                 {/* Mostrar el mensaje inicial si mostrarMensaje es true */}
-                {mostrarMensaje && (
-                    <Text mb="4" fontSize="2xl">
-                        Haz clic en las pelotitas
-                    </Text>
-                )}
+                <motion.div animate={mensajeAnimation} mb="4" fontSize="2xl">
+                    {mostrarMensaje && '¡Dale a las pelotitas un toque mágico con un clic!'}
+                </motion.div>
 
                 {/* Mostrar el contador */}
-                <motion.div initial={{ scale: 2 }} style={{ zIndex: 2, fontSize: '2rem' }}>
-                    Contador: {contador}
+                <motion.div animate={contadorAnimation} fontSize="4rem">
+                    {contador}
                 </motion.div>
 
                 {/* Mostrar las pelotas en posiciones aleatorias */}
                 <Flex mt="4" mb="4" wrap="wrap">
-                    {pelotas.map((pelota) => (
+                    {pelotas.map(({ id, position, color }) => (
                         <motion.div
-                            key={pelota.id}
+                            key={id}
                             whileTap={{ scale: 2 }}
                             style={{
                                 position: 'absolute',
-                                left: pelota.position.x,
-                                top: pelota.position.y,
+                                left: Math.min(window.innerWidth - 40, Math.max(0, position.x)),
+                                top: Math.min(window.innerHeight - 40, Math.max(0, position.y)),
                                 width: '40px',
                                 height: '40px',
-                                background: pelota.color,
+                                background: color,
                                 borderRadius: '50%',
                                 marginRight: '10px',
                                 cursor: 'pointer',
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
+                                zIndex: 3,
                             }}
-                            onClick={() => handleClick(pelota.id)}
-                        >
-                            <span style={{ color: 'white', fontWeight: 'bold', fontSize: '1.5rem' }}>
-                                {pelota.numero}
-                            </span>
-                        </motion.div>
+                            // Manejar clic en la pelota para eliminarla
+                            onClick={() => eliminarPelota(id)}
+                        />
                     ))}
                 </Flex>
             </Flex>
@@ -135,11 +160,15 @@ const ContadorResponsive = () => {
             {/* Botones para incrementar, resetear y decrementar el contador */}
             <Flex mt="4" wrap="wrap">
                 <Flex mt="4" wrap="wrap">
+                    {/* Botón para decrementar */}
                     <Button
                         as={motion.button}
                         onClick={decrementar}
                         whileHover={{ scale: 1.1, backgroundColor: '#e74c3c', color: '#ffffff' }}
                         whileTap={{ scale: 0.9 }}
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
                         colorScheme="red"
                         mx="2"
                         px={{ base: '10', md: '16' }}
@@ -149,11 +178,15 @@ const ContadorResponsive = () => {
                         -
                     </Button>
 
+                    {/* Botón para resetear */}
                     <Button
                         as={motion.button}
                         onClick={reset}
                         whileHover={{ scale: 1.1, backgroundColor: '#999c9b', color: '#ffffff' }}
                         whileTap={{ scale: 0.9 }}
+                        initial={{ opacity: 0, x: 0 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
                         colorScheme="gray"
                         mx="2"
                         px={{ base: '10', md: '16' }}
@@ -162,11 +195,15 @@ const ContadorResponsive = () => {
                         Reset
                     </Button>
 
+                    {/* Botón para incrementar */}
                     <Button
                         as={motion.button}
                         onClick={incrementar}
                         whileHover={{ scale: 1.1, backgroundColor: '#2ecc71', color: '#ffffff' }}
                         whileTap={{ scale: 0.9 }}
+                        initial={{ opacity: 0, x: 100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.5 }}
                         colorScheme="green"
                         mx="2"
                         px={{ base: '10', md: '16' }}
